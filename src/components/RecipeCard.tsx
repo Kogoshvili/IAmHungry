b2cd6b93a5c453d15630abc216af40d074383bbd
Andrian <kogoshvili@pm.me>
2023-06-26 01:35:18 +0400

    card url

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -14,24 +14,27 @@ function RecipeCard({ recipe }: RecipeCardProps) {
     const nutrients = getNutrients(recipe);
 
     return (
-        <Card style={{ height: '100%' }}>
-            <Row gutter={[16, 16]}>
-                <Col span={12}>
-                    <img alt={recipe.label} src={recipe.image} style={{ width: "100%" }} />
-                </Col>
-                <Col span={12}>
-                    <Title level={5} style={{ margin: 0 }}>{recipe.label}</Title>
-                    <p style={{ textAlign: 'start' }}><b>Ingredients</b>: {ingredients}</p>
-                    <p style={{ textAlign: 'start' }}><b>Diet Labels</b>: {dietLabels}</p>
-                    <div style={{ textAlign: 'start' }}>
-                        Calories: {nutrients.calories.total} kcal<br />
-                        Proteins: {nutrients.proteins.total} g<br />
-                        Fats: {nutrients.fats.total} g<br />
-                        Carbs: {nutrients.carbs.total} g<br />
-                    </div>
-                </Col>
-            </Row>
-        </Card>
+        <a href={recipe.url}>
+            <Card style={{ height: '100%' }}>
+                <Row gutter={[16, 16]}>
+                    <Col span={12}>
+                        <img alt={recipe.label} src={recipe.image} style={{ width: "100%" }} />
+                    </Col>
+                    <Col span={12}>
+                        <Title level={5} style={{ margin: 0 }}>{recipe.label}</Title>
+                        <p style={{ textAlign: 'start' }}><b>Ingredients</b>: {ingredients}</p>
+                        <p style={{ textAlign: 'start' }}><b>Diet Labels</b>: {dietLabels}</p>
+                        <div style={{ textAlign: 'start' }}>
+                            <b>Nutrients (total)</b>:<br />
+                            Calories: {nutrients.calories.total} kcal<br />
+                            Proteins: {nutrients.proteins.total} g<br />
+                            Fats: {nutrients.fats.total} g<br />
+                            Carbs: {nutrients.carbs.total} g<br />
+                        </div>
+                    </Col>
+                </Row>
+            </Card>
+        </a>
     );
 }
 
